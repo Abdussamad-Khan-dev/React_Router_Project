821823a050883d872c29ed5758c87ae0b5507b1b
Abdussamad <khanabdussamad32@gmail.com>
2024-05-13 21:42:08 +0530

    react router project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Router } from "react-router-dom"
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
+import Home from "./Components/Home/Home"
+
+function App() {
+ 
+
+  return (
+    <>
+   
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='text-center m-4 text-yellow-600 p-4'>
+      Github Users
+    </div>
+  )
+}
+
+export default Github
